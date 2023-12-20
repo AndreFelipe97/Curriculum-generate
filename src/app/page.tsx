1e917f4ec0769c1129778efbd2dd4c49cbcd69d4
André Felipe Rodrigues de Freitas <andrefeliperf17@gmail.com>
2023-12-19 23:27:04 -0300

    feat: react hook form and validation

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,50 @@
+"use client";
+import { error } from "console";
+import { useEffect } from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
+
+interface IForm {
+  name: string;
+  lastname: string;
+  phone: string;
+  email: string;
+  photo: string;
+  companyOne: string;
+  totalYearsOne: string;
+  activitiesOne: string;
+  companyTwo?: string;
+  totalYearsTwo?: string;
+  activitiesTwo?: string;
+  companyThree?: string;
+  totalYearsThree?: string;
+  activitiesThree?: string;
+  companyFour?: string;
+  totalYearsFour?: string;
+  activitiesFour?: string;
+}
+
 export default function Home() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+
+  useEffect(() => {
+    console.log(errors);
+  }, [errors]);
+
+  const onSubmit = (data: any) => {
+    console.log(errors);
+    console.log(data);
+  };
+
   return (
-    <form className="bg-white rounded-lg mx-8 my-8 p-10 min-h-[75vh] overflow-auto">
+    <form
+      className="bg-white rounded-lg mx-8 my-8 p-10 min-h-[75vh] overflow-auto"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <h1 className="text-slate-900 font-bold text-xl">Seus dados</h1>
       <hr />
       <div className="mt-8 mb-4">
@@ -22,7 +66,13 @@ export default function Home() {
             id="name"
             type="text"
             placeholder="Nome"
+            {...register("name", {
+              required: "Campo obrigatório",
+            })}
           />
+          <p className="mt-2 text-red-500">
+            {errors.name?.message ? String(errors.name?.message) : ""}
+          </p>
         </div>
         <div>
           <label
@@ -36,7 +86,13 @@ export default function Home() {
             id="lastname"
             type="text"
             placeholder="Sobrenome"
+            {...register("lastname", {
+              required: "Campo obrigatório",
+            })}
           />
+          <p className="mt-2 text-red-500">
+            {errors.lastname?.message ? String(errors.lastname?.message) : ""}
+          </p>
         </div>
 
         <div>
@@ -51,7 +107,13 @@ export default function Home() {
             id="phone"
             type="text"
             placeholder="Telefone"
+            {...register("phone", {
+              required: "Campo obrigatório",
+            })}
           />
+          <p className="mt-2 text-red-500">
+            {errors.phone?.message ? String(errors.phone?.message) : ""}
+          </p>
         </div>
 
         <div>
@@ -66,7 +128,13 @@ export default function Home() {
             id="email"
             type="email"
             placeholder="E-mail"
+            {...register("email", {
+              required: "Campo obrigatório",
+            })}
           />
+          <p className="mt-2 text-red-500">
+            {errors.email?.message ? String(errors.email?.message) : ""}
+          </p>
         </div>
         <div>
           <label
@@ -79,7 +147,13 @@ export default function Home() {
             className="shadow appearance-none border rounded w-full py-2 px-3 text-slate-900 leading-tight focus:outline-none focus:shadow-outline"
             id="photo"
             type="file"
+            {...register("photo", {
+              required: "Campo obrigatório",
+            })}
           />
+          <p className="mt-2 text-red-500">
+            {errors.photo?.message ? String(errors.photo?.message) : ""}
+          </p>
         </div>
       </div>
       <div className="mt-8 mb-4">
@@ -89,7 +163,6 @@ export default function Home() {
       </div>
       <hr />
       <div className="grid grid-row-4 xl:grid-cols-2 gap-4 mt-4">
-        {/* Empresa 1 */}
         <div className="flex flex-col gap-4">
           <div>
             <label
@@ -103,37 +176,60 @@ export default function Home() {
               id="companyOne"
               type="text"
               placeholder="Nome da empresa"
+              {...register("companyOne", {
+                required: "Campo obrigatório",
+              })}
             />
+            <p className="mt-2 text-red-500">
+              {errors.companyOne?.message
+                ? String(errors.companyOne?.message)
+                : ""}
+            </p>
           </div>
           <div>
             <label
-              htmlFor="totalYears"
+              htmlFor="totalYearsOne"
               className="block text-slate-900 font-bold text-sm mb-2"
             >
               Total de anos na empresa
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-slate-900 leading-tight focus:outline-none focus:shadow-outline"
-              id="totalYears"
+              id="totalYearsOne"
               type="text"
               placeholder="Ex: 3 / 5 / 10"
+              {...register("totalYearsOne", {
+                required: "Campo obrigatório",
+              })}
             />
+            <p className="mt-2 text-red-500">
+              {errors.totalYearsOne?.message
+                ? String(errors.totalYearsOne?.message)
+                : ""}
+            </p>
           </div>
 
           <div>
             <label
-              htmlFor="activities"
+              htmlFor="activitiesOne"
               className="block text-slate-900 font-bold text-sm mb-2"
             >
               Atividades
             </label>
             <textarea
               className="shadow appearance-none border rounded w-full py-2 px-3 text-slate-900 leading-tight focus:outline-none focus:shadow-outline"
-              id="activities"
+              id="activitiesOne"
+              {...register("activitiesOne", {
+                required: "Campo obrigatório",
+              })}
             />
+            <p className="mt-2 text-red-500">
+              {errors.activitiesOne?.message
+                ? String(errors.activitiesOne?.message)
+                : ""}
+            </p>
           </div>
         </div>
-        {/* Empresa 2 */}
         <div className="flex flex-col gap-4">
           <div>
             <label
@@ -147,37 +243,39 @@ export default function Home() {
               id="companyTwo"
               type="text"
               placeholder="Nome da empresa"
+              {...register("companyTwo")}
             />
           </div>
 
           <div>
             <label
-              htmlFor="totalYears"
+              htmlFor="totalYearsTwo"
               className="block text-slate-900 font-bold text-sm mb-2"
             >
               Total de anos na empresa
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-slate-900 leading-tight focus:outline-none focus:shadow-outline"
-              id="totalYears"
+              id="totalYearsTwo"
               type="text"
               placeholder="Ex: 3 / 5 / 10"
+              {...register("totalYearsTwo")}
             />
           </div>
           <div>
             <label
-              htmlFor="activities"
+              htmlFor="activitiesTwo"
               className="block text-slate-900 font-bold text-sm mb-2"
             >
               Atividades
             </label>
             <textarea
               className="shadow appearance-none border rounded w-full py-2 px-3 text-slate-900 leading-tight focus:outline-none focus:shadow-outline"
-              id="activities"
+              id="activitiesTwo"
+              {...register("activitiesTwo")}
             />
           </div>
         </div>
-        {/* Empresa 3 */}
         <div className="flex flex-col gap-4">
           <div>
             <label
@@ -191,37 +289,39 @@ export default function Home() {
               id="companyThree"
               type="text"
               placeholder="Nome da empresa"
+              {...register("companyThree")}
             />
           </div>
 
           <div>
             <label
-              htmlFor="totalYears"
+              htmlFor="totalYearsThree"
               className="block text-slate-900 font-bold text-sm mb-2"
             >
               Total de anos na empresa
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-slate-900 leading-tight focus:outline-none focus:shadow-outline"
-              id="totalYears"
+              id="totalYearsThree"
               type="text"
               placeholder="Ex: 3 / 5 / 10"
+              {...register("totalYearsThree")}
             />
           </div>
           <div>
             <label
-              htmlFor="activities"
+              htmlFor="activitiesThree"
               className="block text-slate-900 font-bold text-sm mb-2"
             >
               Atividades
             </label>
             <textarea
               className="shadow appearance-none border rounded w-full py-2 px-3 text-slate-900 leading-tight focus:outline-none focus:shadow-outline"
-              id="activities"
+              id="activitiesThree"
+              {...register("activitiesThree")}
             />
           </div>
         </div>
-        {/* Empresa 4 */}
         <div className="flex flex-col gap-4">
           <div>
             <label
@@ -235,33 +335,36 @@ export default function Home() {
               id="companyFour"
               type="text"
               placeholder="Nome da empresa"
+              {...register("companyFour")}
             />
           </div>
 
           <div>
             <label
-              htmlFor="totalYears"
+              htmlFor="totalYearsFour"
               className="block text-slate-900 font-bold text-sm mb-2"
             >
               Total de anos na empresa
             </label>
             <input
               className="shadow appearance-none border rounded w-full py-2 px-3 text-slate-900 leading-tight focus:outline-none focus:shadow-outline"
-              id="totalYears"
+              id="totalYearsFour"
               type="text"
               placeholder="Ex: 3 / 5 / 10"
+              {...register("totalYearsFour")}
             />
           </div>
           <div>
             <label
-              htmlFor="activities"
+              htmlFor="activitiesFour"
               className="block text-slate-900 font-bold text-sm mb-2"
             >
               Atividades
             </label>
             <textarea
               className="shadow appearance-none border rounded w-full py-2 px-3 text-slate-900 leading-tight focus:outline-none focus:shadow-outline"
-              id="activities"
+              id="activitiesFour"
+              {...register("activitiesFour")}
             />
           </div>
         </div>
